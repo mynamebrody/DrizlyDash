25e5197bfc688b4b3f7ff487272efd3fef200864
Brody Berson <brody.berson@me.com>
2015-10-12 18:43:48 -0600

    Added to the Setup to query store

--- setup.js
+++ setup.js
@@ -27,13 +27,27 @@ prompt.get([{
     partner_token: process.env.PARTNER_TOKEN,
     email: result.email,
     password: result.password
-};
+  };
   request.post({url: endpoint, formData: formData}, function optionalCallback(err, httpResponse, body) {
-  if (err) {
-    return console.error('Login failed:', err);
-  }
-  console.log('Login successful!  Server responded with: \n', JSON.parse(body));
+    if (err) {
+      return console.error('Login failed:', err);
+    }
+    console.log('Login successful!  Server responded with: \n', JSON.parse(body));
+  });
 });
+
+prompt.get([{
+  name:'query',
+  description: 'Enter your alcohol query'
+  }], function (err, result) {
+  console.log('Command-line input received:');
+  console.log('  query: ' + result.query);
+
+  var endpoint = process.env.URL + '/catalog/filter?partner_token=' + process.env.PARTNER_TOKEN + '&token=' + process.env.TOKEN + '&store_id=92&q=' + result.query;
+
+  request.get(endpoint).on('response', function(response) {
+    console.log(response);
+  });
 });
 
 function onErr(err) {
